09042eb65bfaf427c62142c7745aa0053aecb0a1
Felix Palludan Hargreaves <felix.palludan.hargreaves@LEGO.com>
2017-02-25 10:46:00 +0100

    Secrets table

--- server/src/main/scala/com/lambdaminute/wishr/persistence/InMemoryH2.scala
+++ server/src/main/scala/com/lambdaminute/wishr/persistence/InMemoryH2.scala
@@ -1,8 +1,9 @@
 package com.lambdaminute.wishr.persistence
 
+import cats.free.Free
 import com.github.t3hnar.bcrypt._
 import com.lambdaminute.wishr.model.{CreateUserRequest, WishEntry}
-import doobie.free.connection.ConnectionIO
+import doobie.free.connection.{ConnectionIO, ConnectionOp}
 import doobie.imports._
 import org.h2.tools.Server
 
@@ -13,25 +14,36 @@ case class InMemoryH2(dbConfigKey: String, port: Int) extends Persistence[String
 //  val xa = DriverManagerTransactor[IOLite]("jdbc:h2:mem:test;DB_CLOSE_DELAY=-1", "sa", "")
   val xa = DriverManagerTransactor[IOLite](
     "org.h2.Driver",
-    "jdbc:h2:mem:test;DB_CLOSE_DELAY=-1",
+    "jdbc:h2:mem:test;DB_CLOSE_DELAY=-1;MODE=MySQL",
+//    "jdbc:h2:mem:test;DB_CLOSE_DELAY=-1",
     "sa",
     ""
   )
 
-  val create: Update0 =
+  val createUsersTable: Update0 =
     sql"""
     CREATE TABLE users (
-      firstName       VARCHAR,
-      lastName        VARCHAR,
-      email           VARCHAR     UNIQUE,
-      hashedPassword   VARCHAR,
-      registrationToken VARCHAR,
-      finalized  BOOLEAN
+      firstName          VARCHAR,
+      lastName           VARCHAR,
+      email              VARCHAR UNIQUE,
+      hashedPassword     VARCHAR,
+      registrationToken  VARCHAR,
+      finalized          BOOLEAN
       )
   """.update
 
-  val createResult: Int = create.run.transact(xa).unsafePerformIO
-  println(s"Createresult: $createResult")
+  val createSecretsTable: Update0 = sql"""
+    CREATE TABLE secrets (
+       user             VARCHAR UNIQUE,
+       secret           VARCHAR,
+       expirationDate   TIMESTAMP
+      )
+   """.update
+
+  val createUsersResult: Int = createUsersTable.run.transact(xa).unsafePerformIO
+  println(s"Create users: $createUsersResult")
+  val createSecretsResult: Int = createSecretsTable.run.transact(xa).unsafePerformIO
+  println(s"Create secrets: $createSecretsResult")
 
   case class UserPass(firstName: String, hashedPassword: String)
 
@@ -39,20 +51,46 @@ case class InMemoryH2(dbConfigKey: String, port: Int) extends Persistence[String
     val selectUser =
       sql"""SELECT firstName, hashedPassword FROM users WHERE firstName=$user""".query[UserPass]
 
-    println(s"Trying password: $hash against db...")
-
     val userPass: List[UserPass] = selectUser.list.transact(xa).unsafePerformIO
     userPass match {
-      case UserPass(_, storedHash) :: Nil if hash.isBcrypted(storedHash) => Right("fake token")
-      case _ => Left("Bad user credentials")
+      case UserPass(_, storedHash) :: Nil if hash.isBcrypted(storedHash) => getOrCreateSecretFor(user)
+      case _                                                             => Left("Bad user credentials")
+    }
+  }
+
+  private def newSecret = java.util.UUID.randomUUID.toString
+  private def getOrCreateSecretFor(user: String): Either[String, String] = {
+
+    val insertOrUpdate =
+      sql"""
+    INSERT INTO secrets (user, secret, expirationDate) VALUES ($user, $newSecret, DATEADD('SECOND',60,
+    CURRENT_TIMESTAMP()))
+                      ON DUPLICATE KEY UPDATE expirationDate=DATEADD('SECOND',60, CURRENT_TIMESTAMP())
+         """.update.run
+
+    val token: Free[ConnectionOp, (Int, String)] = for {
+      updateCount <- insertOrUpdate
+      token       <- sql"SELECT secret FROM secrets WHERE user=$user".query[String].unique
+    } yield (updateCount, token)
+
+    token.transact(xa).unsafePerformIO match {
+      case (1, token) => Right(token)
+      case _          => Left(s"Unable to update and retrieve token for $user")
     }
   }
 
   override def getSecretFor(user: String): Either[String, String] = ???
 
-  override def getUserFor(secret: String): Either[String, String] = ???
+  override def getUserFor(secret: String): Either[String, String] = {
+    val userFor =
+      sql"SELECT user FROM secrets WHERE secret=$secret AND now() < expirationDate"
+      .query[String]
+      .option
+      .map(_.toRight("Token expired"))
+    userFor.transact(xa).unsafePerformIO
+  }
 
-  override def getEntriesFor(user: String): Either[String, List[WishEntry]] = ???
+  override def getEntriesFor(user: String): Either[String, List[WishEntry]] = Right(Nil)
 
   override def set(entries: List[WishEntry]): Either[String, String] = ???
 
@@ -66,7 +104,7 @@ case class InMemoryH2(dbConfigKey: String, port: Int) extends Persistence[String
     val c                              = createUserRequest
     val dbu                            = DBUser(c.firstName, c.lastName, c.email, c.password.bcrypt, activationToken, false)
     val insertQuery: ConnectionIO[Int] = up.run(dbu)
-    val result: Int = insertQuery.transact(xa).unsafePerformIO
+    val result: Int                    = insertQuery.transact(xa).unsafePerformIO
     result match {
       case 1 => Right("Successfully created user")
       case _ => Left("Failed creating user in db")
